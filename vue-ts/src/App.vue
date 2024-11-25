775cea04784cf8c0c675eb830264460e4c2d0924
Ardelan <ardelanyamanel@outlook.de>
2024-11-25 22:04:54 +0100

    feat(index.scss): update reset.css

--- vue-ts/src/App.vue
+++ vue-ts/src/App.vue
@@ -7,13 +7,12 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: {}
 });
 </script>
 
 <style lang="scss">
-@use 'utils.scss';
-@use 'transitions.scss';
+@import url('transitions.scss');
 
 *,
 *::after,
@@ -21,19 +20,27 @@ export default defineComponent({
   margin: 0;
   padding: 0;
   user-select: none;
-  box-sizing: border-box;
+  box-sizing: border-box !important;
   -webkit-user-drag: none;
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-rendering: optimizeLegibility;
 }
 
 body,
 html {
   width: 100vw;
   height: 100vh;
+
+  overflow: hidden;
 }
 
 body {
   display: grid;
   place-items: center;
+
+  line-height: 1;
 }
 
 a,
@@ -60,15 +67,5 @@ button:hover {
 ::-webkit-inner-spin-button,
 ::-webkit-outer-spin-button {
   display: none;
-}
-
-.scroll-drag {
-  &:hover {
-    cursor: -webkit-grab;
-  }
-
-  &:active {
-    cursor: -webkit-grabbing;
-  }
 }
 </style>
